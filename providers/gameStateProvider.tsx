c63fbbbbe6b708f1ad030ac7de552435c3925671
ryota-09 <n.ryo950911@gmail.com>
2022-07-02 15:23:26 +0900

    gameStateProviderの作成

---
+++ providers/gameStateProvider.tsx
@@ -0,0 +1,56 @@
+import { createContext, Dispatch, FC, ReactNode, useReducer } from "react";
+import { GameState } from "../types/types";
+
+type State = {
+  gameState: GameState;
+};
+
+type Action = {
+  type: "SET_GAMESTATE";
+  payload: {
+    gameState?: GameState;
+  };
+};
+
+export type GameStateContextType = {
+  gameState: State;
+  setGameState: Dispatch<Action>;
+};
+
+export const gameStateContext = createContext({} as GameStateContextType);
+
+const initialState: State = {
+  gameState: {
+    id: "",
+    user_id: "",
+    trial_time: 0,
+    correct_count: 0,
+    vocabulary_point: 0,
+    total_point: 0,
+    created_at: null,
+    current_index: 0,
+    word_list: [],
+  },
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "SET_GAMESTATE":
+      return { gameState: action.payload.gameState };
+    default:
+      return state;
+  }
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+export const GameStateProvider: FC<Props> = ({ children }) => {
+  const [gameState, setGameState] = useReducer(reducer, initialState);
+  return (
+    <gameStateContext.Provider value={{ gameState, setGameState }}>
+      {children}
+    </gameStateContext.Provider>
+  );
+};
