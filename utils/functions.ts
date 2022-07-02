6af66e0dcc0c610bb13db5ed8a25553fabbb2c44
ryota-09 <n.ryo950911@gmail.com>
2022-07-02 15:55:57 +0900

    makeDraggableTextArray

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,5 @@
+import { v4 as uuid } from "uuid";
+
 import { DraggableText } from "../types/types";
 
 export const makeSlicedTextAray = (targetText: string): string[] => {
@@ -6,12 +8,24 @@ export const makeSlicedTextAray = (targetText: string): string[] => {
   return newArray;
 };
 
-export const shuffleArray = (targetArray: string[]) => {
+export const shuffleArray = (targetArray: string[]): string[] => {
   const copiedArray = targetArray.slice();
   for (let i = copiedArray.length - 1; i >= 0; i--) {
     const rand = Math.floor(Math.random() * (i + 1));
     [copiedArray[i], copiedArray[rand]] = [copiedArray[rand], copiedArray[i]];
   }
-
   return copiedArray;
+};
+
+export const makeDraggableTextArray = (targetArray: string[]): DraggableText[] => {
+  let newObj: DraggableText;
+  let newArray: DraggableText[];
+  for (let singleText of targetArray) {
+    newObj = {
+      id: uuid(),
+      singleText: singleText,
+    };
+    newArray.push(newObj);
+  }
+  return newArray;
 };
