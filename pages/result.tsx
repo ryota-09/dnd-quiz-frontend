0222b4a5708e2a911fbf4ea9d6704aa0cd4cd186
ryota-09 <n.ryo950911@gmail.com>
2022-07-04 14:56:26 +0900

    resultページデータ受け渡し実装

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,11 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { NextPage } from "next";
-import { useEffect } from "react";
-import { v4 as uuid } from "uuid";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Layout from "../components/organisms/Layout";
 
 import { useGameState } from "../hooks/useGameState";
 
 const Result: NextPage = () => {
   const { gameState, setGameState } = useGameState();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
 
   const calcTotalPoint = (
     trialTime: number,
@@ -25,19 +31,18 @@ const Result: NextPage = () => {
     }
 
     // correctPoint
-    if (correctCount < 3) {
-      correctPoint = correctCount * 10;
-    } else if (correctCount >= 3 && correctCount <= 4) {
-      correctPoint = correctCount * 10 + 15;
-    } else {
+    if (correctCount === 5) {
       correctPoint = 70;
+    } else if (correctCount >= 3 && correctCount <= 4) {
+      correctPoint = correctCount * 5 + 15;
+    } else {
+      correctPoint = correctCount * 10;
     }
     // vocaPoint
     vocaPoint = vocabularyPoint;
     return timePoint + correctPoint + vocaPoint;
   };
 
-  
   useEffect(() => {
     setGameState({
       type: "SET_GAMESTATE",
@@ -62,12 +67,40 @@ const Result: NextPage = () => {
   }, []);
   return (
     <>
-      <div>
-        <div>結果発表</div>
-        <p>{gameState.correct_count}</p>
-        <p>{gameState.vocabulary_point}</p>
-        <p>{gameState.total_point}</p>
-      </div>
+      <Layout title="結果発表">
+        <button
+          className="inline-block bg-green-400 hover:bg-green-600 active:bg-indigo-700 focus-visible:ring ring-green-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          結果を表示する
+        </button>
+        <div className="flex justify-center items-center flex-col min-h-screen">
+          {isOpen ? (
+            <div>
+              <p className="text-black-800 text-9xl md:text-4xl font-bold">
+                タイム: {gameState.trial_time} s
+              </p>
+              <p className="text-black-800 text-9xl md:text-4xl font-bold">
+                正解数: {gameState.correct_count} / 5
+              </p>
+              <p className="text-black-800 text-9xl md:text-4xl font-bold">
+                語い力: {gameState.vocabulary_point} pt
+              </p>
+              <p className="text-red-800 text-9xl md:text-4xl font-bold">
+                総合点: {gameState.total_point} pt
+              </p>
+              <button
+          className="inline-block bg-green-400 hover:bg-green-600 active:bg-indigo-700 focus-visible:ring ring-green-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+          onClick={() => router.push("/quiz")}
+        >
+          リトライ
+        </button>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </Layout>
     </>
   );
 };
