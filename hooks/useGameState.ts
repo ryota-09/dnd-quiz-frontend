d844d426baff25fbff7015cf089523f51724f587
ryota-09 <n.ryo950911@gmail.com>
2022-07-08 16:31:28 +0900

    game関連のクエリの作成

--- hooks/useGameState.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { gameStateContext, GameStateContextType } from "../providers/gameStateProvider";
-
-export const useGameState = (): GameStateContextType => {
-  return useContext(gameStateContext);
-}
--- queries/queries.ts
+++ queries/queries.ts
@@ -29,3 +29,73 @@ export const CREATE_WORD = gql`
     }
   }
 `;
+
+export const GET_ALL_GAMES = gql`
+  query GetAllGames {
+    games {
+      id
+      user_id
+      trial_time
+      correct_count
+      vocabulary_point
+      total_point
+      created_at
+    }
+  }
+`;
+
+export const GET_GAMES_TOP_THREE = gql`
+  query GetGamesTopThree {
+    games(first: 3) {
+      id
+      user_id
+      trial_time
+      correct_count
+      vocabulary_point
+      total_point
+      created_at
+    }
+  }
+`;
+
+export const GET_GAMES_BY_USERID = gql`
+  query GetGamesByUserId($userId: uuid!) {
+    getGameListByUerId(userId: $userId) {
+      id
+      user_id
+      trial_time
+      correct_count
+      vocabulary_point
+      total_point
+      created_at
+    }
+  }
+`;
+
+export const CREATE_GAME = gql`
+  mutation createGame(
+    $user_id: uuid!
+    $trial_time: Float!
+    $correct_count: Float!
+    $vocabulary_point: Float!
+    $total_point: Float!
+  ) {
+    createGame(
+      gameInput: {
+        user_id: $user_id
+        trial_time: $trial_time
+        correct_count: $correct_count
+        vocabulary_point: $vocabulary_point
+        total_point: $total_point
+      }
+    ) {
+      id
+      user_id
+      trial_time
+      correct_count
+      vocabulary_point
+      total_point
+      created_at
+    }
+  }
+`;
